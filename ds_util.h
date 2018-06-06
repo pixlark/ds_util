0ef7f306a0ec71ff55edf986fa305ccb0056c638
Paul Tilley <t1000.pixlark@gmail.com>
2018-06-06 07:19:25 -0400

    license

--- ds_util.h
+++ ds_util.h
@@ -1,3 +1,7 @@
+// This software is dual-licensed to the public domain and under the following
+// license: you are granted a perpetual, irrevocable license to copy, modify,
+// publish, and distribute this file as you see fit.
+
 #ifndef DS__INCLUDE_DS_UTIL_H
 #define DS__INCLUDE_DS_UTIL_H
 
