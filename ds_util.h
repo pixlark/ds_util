c20cfa16e45be387be16c801bc2d33204c96acf4
Paul Tilley <t1000.pixlark@gmail.com>
2018-06-08 09:58:59 -0400

    slight refactor + changing inserted value with insert

--- ds_util.h
+++ ds_util.h
@@ -172,6 +172,7 @@ struct HashTable {
 	void init(int size, int (*hash_func)(K,int), bool (*key_comp)(K,K));
 	void insert(K key, V value);
 	int  index(K key, V * value);
+	int locate(K key);
 };
 
 template <typename K, typename V>
@@ -187,11 +188,27 @@ void HashTable<K,V>::init(int size, int (*hash_func)(K,int), bool (*key_comp)(K,
 }
 
 template <typename K, typename V>
+int HashTable<K,V>::locate(K key)
+{
+	int position = hash_func(key, table_size);
+	int counter = 0;
+	while (1) {
+		if (!table[position].filled) return -1;
+		if (key_comp(table[position].key, key)) break;
+		
+		if (counter++ >= table_size) return -1;
+		position = (position + 1) % table_size;
+	}
+	return position;
+}
+
+template <typename K, typename V>
 void HashTable<K,V>::insert(K key, V value)
 {
 	int position = hash_func(key, table_size);
 	int counter = 0;
 	while (table[position].filled) {
+		if (key_comp(table[position].key, key)) break;
 		assert(counter++ < table_size); // Don't exceed maximum table size
 		position = (position + 1) % table_size;
 	}
@@ -203,15 +220,8 @@ void HashTable<K,V>::insert(K key, V value)
 template <typename K, typename V>
 int HashTable<K,V>::index(K key, V * value)
 {
-	int position = hash_func(key, table_size);
-	int counter = 0;
-	while (1) {
-		if (!table[position].filled) return 1;
-		if (key_comp(table[position].key, key)) break;
-		
-		if (counter++ >= table_size) return 1;
-		position = (position + 1) % table_size;
-	}
+	int position = locate(key);
+	if (position == -1) return 1;
 	if (value != NULL) *value = table[position].value;
 	return 0;
 }
